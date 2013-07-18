4caf750a19e31d2459bbcd62a8a0426aa8d887e4
Cody Rushing <codyrushing@gmail.com>
2013-07-18 01:44:04 -0500

    "setting up a basic socket test for hangout":

--- js/app-browsing.js
+++ js/app-browsing.js
@@ -1,12 +1,12 @@
 $(function(){
-	var socket = io.connect("http://localhost:3000");
+	var socket = io.connect("http://ec2-23-22-44-238.compute-1.amazonaws.com:80");
 
 	socket.on("serverEventTest", function(data){
 		console.log(data.payload);
 	});
 
-	$("button").click(function(e){
-		socket.emit("clientEventTest", {payload: $("input").val()});
+	$("button#socket-go").click(function(e){
+		socket.emit("clientEventTest", {payload: $("input[name=\"socket-test\"]").val()});
 	});
 
 });
